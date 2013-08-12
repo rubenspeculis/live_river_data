924601bc5cf578550cabaa6739337cbd9db941a6
Rubens Peculis <rubens.peculis@mdba.gov.au>
2013-08-12 15:52:15 +1000

    Includes update

--- includes/helper.php
+++ includes/helper.php
@@ -18,17 +18,19 @@ function dump($variable) {
 function cleanInputFile($filename){
   if(file_exists($filename)){
     $file = file_Get_contents($filename);
-
+    
     // Strip header information
     $needle = 'JSON string:';
     $cleaning = strstr($file, $needle);
-    $first_instance = '{';
-    $cleaning = strstr($cleaning, $first_instance);
-
-    // Strip footer information
-    $last_instance = strrpos($cleaning, '}');
-    $json_string = substr_replace($cleaning, '', $last_instance+1, strlen($cleaning));
-
+    if($cleaning){
+      $first_instance = '{';
+      $cleaning = strstr($cleaning, $first_instance);
+      // Strip footer information
+      $last_instance = strrpos($cleaning, '}');
+      $json_string = substr_replace($cleaning, '', $last_instance+1, strlen($cleaning));
+    } else {
+      $json_string = $file;
+    }
     // Seup JSON Object
     if(isJson($json_string)){
       $json = json_decode($json_string);
